5a28ae57a3667c6a8f7eef0f0923ca30d48961ea
lichaunlin <lichaunlin@gmail.com>
2022-05-24 18:15:57 +0800

    update

--- godb/model.go
+++ godb/model.go
@@ -13,6 +13,33 @@ func (tis *Model) GetID() uint {
 	return tis.ID
 }
 
+func (tis *Model) GetUID() string {
+	if tis == nil || tis.Uid == nil {
+		return ""
+	}
+
+	return *tis.Uid
+}
+
+func (tis *Model) GetCreatedTime() time.Time {
+	if tis == nil || tis.Uid == nil {
+		return time.Unix(0, 0)
+	}
+
+	return *tis.CreatedAt
+}
+
+func (tis *Model) GetUpdatedTime() time.Time {
+	if tis == nil || tis.Uid == nil {
+		return time.Unix(0, 0)
+	}
+
+	return *tis.UpdatedAt
+}
+
 type InterfaceModel interface {
 	GetID() uint
+	GetUID() string
+	GetCreatedTime() time.Time
+	GetUpdatedTime() time.Time
 }
