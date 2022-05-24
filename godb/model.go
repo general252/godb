e8a3f1b94b2a1409aa1b7667e360e18752dd434f
lichaunlin <lichaunlin@gmail.com>
2022-05-24 10:50:36 +0800

    size

--- godb/model.go
+++ godb/model.go
@@ -4,7 +4,7 @@ import "time"
 
 type Model struct {
 	ID        uint
-	Uid       *string `gorm:"column:uid;type:string;uniqueIndex;not null"` // 唯一索引
+	Uid       *string `gorm:"column:uid;type:string;uniqueIndex;size:64;not null"` // 唯一索引
 	CreatedAt *time.Time
 	UpdatedAt *time.Time
 }
